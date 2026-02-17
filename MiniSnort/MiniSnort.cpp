166f317917facf30aad41975a8b32a88cbd3455a
Rotem Shitrit <rotems066@gmail.com>
2026-02-17 17:28:05 +0200

    Refactored parsing into PacketPipeline orchestrator. Introduced ParsedPacket structure. Separated parsing logic from printing (threading-ready architecture).

--- MiniSnort/MiniSnort.cpp
+++ MiniSnort/MiniSnort.cpp
@@ -5,6 +5,19 @@
 void PrintInterfaces(const pcap_if_t* alldevs);
 const pcap_if_t* UserChoice(const pcap_if_t* alldevs);
 void PrintPacket(u_char* user, const pcap_pkthdr* packetHeader, const u_char* packetData);
+struct ParsedPacket {
+    uint16_t etherType = 0;
+    bool hasIPv4 = false;
+    IPv4Info ipv4{};
+    enum class L4Type{None, TCP, UDP, ICMP, Other} l4Type = L4Type :: None;
+    bool hasTcp = false;
+    TcpInfo tcp{};
+    bool hasUdp = false;
+    UdpInfo udp{};
+    bool hasIcmp = false;
+    IcmpInfo icmp{};
+};
+bool PacketPipeline(const pcap_pkthdr* packetHeader, const u_char* packetData, Praser& praser, ParsedPacket& outPacket);
 static void PrintIPv4(uint32_t ip)
 {
     std::cout
@@ -81,91 +94,103 @@ void PrintPacket(u_char* user,const pcap_pkthdr* packetHeader, const u_char* pac
         << " caplen: " << packetHeader->caplen
         << " TimeStamp: " << packetHeader->ts.tv_sec;
 
-    // Ethernet minimum
-    if (!praser->CheckLimit(packetHeader, 0, 14)) {
-        std::cout << "\nPacket too small for Ethernet header\n";
+    ParsedPacket parsed;
+    bool ok = PacketPipeline(packetHeader, packetData, *praser, parsed);
+    if (!ok) {
+        std::cout << "\nParse failed\n";
         std::cout << "-------------------------------------------------------------------------\n";
         return;
     }
 
-    uint16_t etherType = praser->EtherType(packetData);
-
     std::cout << " EtherType: ";
-    if (etherType == 0x0800) {
+
+    if (parsed.etherType == 0x0800) {
         std::cout << "IPv4";
 
-        // L3 starts after Ethernet header
-        int l3Offset = 14;
-
-        IPv4Info ipv4;
-        if (!praser->ReadIPv4Info(packetHeader, packetData, l3Offset, ipv4)) {
+        if (!parsed.hasIPv4) {
             std::cout << "\nBad/Truncated IPv4 header\n";
             std::cout << "-------------------------------------------------------------------------\n";
             return;
         }
 
         std::cout << "\nIPv4 Src: ";
-        PrintIPv4(ipv4.srcIP);
+        PrintIPv4(parsed.ipv4.srcIP);
         std::cout << " Dst: ";
-        PrintIPv4(ipv4.dstIP);
-        std::cout << " Protocol: " << int(ipv4.protocol);
+        PrintIPv4(parsed.ipv4.dstIP);
+        std::cout << " Protocol: " << int(parsed.ipv4.protocol);
 
-        // L4 dispatch by protocol
-        if (ipv4.protocol == 6) { // TCP
-            TcpInfo tcp;
-            if (!praser->ReadTCPInfo(packetHeader, packetData, ipv4.l4Offset, tcp)) {
-                std::cout << "\nBad/Truncated TCP header\n";
-                std::cout << "-------------------------------------------------------------------------\n";
-                return;
-            }
-
-            std::cout << "\nTCP SrcPort: " << tcp.srcPort
-                << " DstPort: " << tcp.dstPort
-                << " Flags: 0x" << std::hex << int(tcp.flags) << std::dec
-                << " HeaderLen: " << int(tcp.headerLength)
-                << " PayloadOffset: " << tcp.payloadOffset;
+        if (parsed.l4Type == ParsedPacket::L4Type::TCP && parsed.hasTcp) {
+            std::cout << "\nTCP SrcPort: " << parsed.tcp.srcPort
+                << " DstPort: " << parsed.tcp.dstPort
+                << " Flags: 0x" << std::hex << int(parsed.tcp.flags) << std::dec
+                << " HeaderLen: " << int(parsed.tcp.headerLength)
+                << " PayloadOffset: " << parsed.tcp.payloadOffset;
         }
-        else if (ipv4.protocol == 17) { // UDP
-            UdpInfo udp;
-            if (!praser->ReadUDPInfo(packetHeader, packetData, ipv4.l4Offset, udp)) {
-                std::cout << "\nBad/Truncated UDP header\n";
-                std::cout << "-------------------------------------------------------------------------\n";
-                return;
-            }
-
-            std::cout << "\nUDP SrcPort: " << udp.srcPort
-                << " DstPort: " << udp.dstPort
-                << " Length: " << udp.length
-                << " HeaderLen: " << int(udp.headerLength)
-                << " PayloadOffset: " << udp.payloadOffset;
+        else if (parsed.l4Type == ParsedPacket::L4Type::UDP && parsed.hasUdp) {
+            std::cout << "\nUDP SrcPort: " << parsed.udp.srcPort
+                << " DstPort: " << parsed.udp.dstPort
+                << " Length: " << parsed.udp.length
+                << " HeaderLen: " << int(parsed.udp.headerLength)
+                << " PayloadOffset: " << parsed.udp.payloadOffset;
         }
-        else if (ipv4.protocol == 1) { // ICMP
-            IcmpInfo icmp;
-            if (!praser->ReadICMPInfo(packetHeader, packetData, ipv4.l4Offset, icmp)) {
-                std::cout << "\nBad/Truncated ICMP header\n";
-                std::cout << "-------------------------------------------------------------------------\n";
-                return;
-            }
-
-            std::cout << "\nICMP Type: " << int(icmp.type)
-                << " Code: " << int(icmp.code)
-                << " Checksum: 0x" << std::hex << icmp.checksum << std::dec
-                << " HeaderLen: " << int(icmp.headerLength)
-                << " PayloadOffset: " << icmp.payloadOffset;
+        else if (parsed.l4Type == ParsedPacket::L4Type::ICMP && parsed.hasIcmp) {
+            std::cout << "\nICMP Type: " << int(parsed.icmp.type)
+                << " Code: " << int(parsed.icmp.code)
+                << " Checksum: 0x" << std::hex << parsed.icmp.checksum << std::dec
+                << " HeaderLen: " << int(parsed.icmp.headerLength)
+                << " PayloadOffset: " << parsed.icmp.payloadOffset;
         }
-        else {
-            std::cout << "\nUnsupported IPv4 L4 protocol: " << int(ipv4.protocol);
+        else if (parsed.l4Type == ParsedPacket::L4Type::Other) {
+            std::cout << "\nUnsupported IPv4 L4 protocol: " << int(parsed.ipv4.protocol);
         }
     }
-    else if (etherType == 0x0806) {
+    else if (parsed.etherType == 0x0806) {
         std::cout << "ARP";
     }
-    else if (etherType == 0x86DD) {
+    else if (parsed.etherType == 0x86DD) {
         std::cout << "IPv6";
     }
     else {
-        std::cout << "Unknown (0x" << std::hex << etherType << std::dec << ")";
+        std::cout << "Unknown (0x" << std::hex << parsed.etherType << std::dec << ")";
     }
 
     std::cout << "\n-------------------------------------------------------------------------\n";
+}
+bool PacketPipeline(const pcap_pkthdr* packetHeader, const u_char* packetData, Praser& praser, ParsedPacket& outPacket) {
+    //safety check + ether type check
+    if (!praser.CheckLimit(packetHeader, 0, 14)) return false;
+    outPacket.etherType = praser.EtherType(packetData);
+    if (outPacket.etherType != 0x0800) return true;
+    int l3Offset = 14;
+    if (!praser.ReadIPv4Info(packetHeader, packetData, l3Offset, outPacket.ipv4))
+        return false;
+
+    outPacket.hasIPv4 = true;
+    //if IPv4 read the right protocol
+    if (outPacket.ipv4.protocol == 6) {
+        if (!praser.ReadTCPInfo(packetHeader, packetData, outPacket.ipv4.l4Offset, outPacket.tcp))
+            return false;
+
+        outPacket.l4Type = ParsedPacket::L4Type::TCP;
+        outPacket.hasTcp = true;
+    }
+    else if (outPacket.ipv4.protocol == 17) {
+        if (!praser.ReadUDPInfo(packetHeader, packetData, outPacket.ipv4.l4Offset, outPacket.udp))
+            return false;
+
+        outPacket.l4Type = ParsedPacket::L4Type::UDP;
+        outPacket.hasUdp = true;
+    }
+    else if (outPacket.ipv4.protocol == 1) {
+        if (!praser.ReadICMPInfo(packetHeader, packetData, outPacket.ipv4.l4Offset, outPacket.icmp))
+            return false;
+
+        outPacket.l4Type = ParsedPacket::L4Type::ICMP;
+        outPacket.hasIcmp = true;
+    }
+    else {
+        outPacket.l4Type = ParsedPacket::L4Type::Other;
+    }
+    return true;
+
 }
