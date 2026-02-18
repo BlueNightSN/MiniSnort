9176e15d6812c1c77af294a8d6140665861754eb
Rotem Shitrit <rotems066@gmail.com>
2026-02-18 18:34:01 +0200

    started Guard added to him Severity and Alert struct and rules

--- MiniSnort/Guard.h
+++ MiniSnort/Guard.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+#include <optional>
+enum class Severity {Info,Low,Medium,High};
+struct Alert {
+	Severity severity;
+	std::string ruleName;
+	std::string message;
+};
+struct ParsedPacket;
+class IRule {
+public:
+	virtual ~IRule() = default;
+	virtual std::optional<Alert> Evaluate(const ParsedPacket& packet) const = 0;
+};
+class Guard
+{
+	
+};
